85b67d6d594c8a9da0c6271c9ffcdcaa81dba6e8
Ivan Kelembetov <john-1123@narod.ru>
2024-02-18 15:30:13 +0300

    Button onclick

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('test')
+"use strict";
+var amount = document.getElementById('amount').value
+var button = document.getElementById('refresh')
+var results = document.getElementById('results')
+button.addEventListener("click", function() { 
+    console.log(amount)
+})
