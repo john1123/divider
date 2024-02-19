6b9a36ded1f58ea7ff2ed071ee964c30bd7e734b
Ivan Kelembetov <john-1123@narod.ru>
2024-02-19 15:08:03 +0300

    First version

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ var amount = document.getElementById('amount').value
 var button = document.getElementById('refresh')
 var results = document.getElementById('results')
 button.addEventListener("click", function() { 
-    console.log(amount)
+    var out = ""
+    out += "10% от суммы составляют: " + "<br />\n"
+    out += "90% от суммы составляют: " + amount
+    results.innerHTML = out
 })
