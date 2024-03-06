8e0ce98b7794eff189bcb91b12fb38060a4fcdcd
Ivan Kelembetov <john-1123@narod.ru>
2024-03-06 07:41:12 +0300

    Changed variable prefixies from var; removed strict mode

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-"use strict";
 document.getElementById("amount").focus()
 const btn = document.getElementsByTagName("input")[1];
 
 btn.addEventListener("click", function() { 
-    var amnt = document.getElementById('amount').value;
-    var results = document.getElementById('results');
-    var out = "";
+    const amnt = document.getElementById('amount').value;
+    const results = document.getElementById('results');
+    let out = "";
     out += "10% от суммы составляют: " + (10 * amnt / 100) + "<br />\n";
     out += "90% от суммы составляют: " + (90 * amnt / 100);
     results.innerHTML = out;
-    console.log(amnt);
 })
